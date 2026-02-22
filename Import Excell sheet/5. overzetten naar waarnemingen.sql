472ff8ff863a832ce0ab084fb59a5506dc7e4359
Ton2241 <touwen-veeboeren-5w@icloud.com>
2026-02-22 22:03:32 +0100

    de rest

--- Import Excell sheet/5. overzetten naar waarnemingen.sql
+++ Import Excell sheet/5. overzetten naar waarnemingen.sql
@@ -1,5 +1,5 @@
-INSERT INTO waarnemingen (plot_id, euring_code, jaar, territoria)
-SELECT t.plot_id, t.euring_code, t.jaar, t.territoria
-FROM temp_waarnemingen_lang t
+INSERT INTO waarnemingen (plot_id, soort_id, jaar, territoria, bron)
+SELECT t.plot_id, t.soort_id, t.jaar, t.territoria, t.bron
+FROM import_waarnemingen_lang t
 INNER JOIN plot_jaar_oppervlak pjo 
     ON t.plot_id = pjo.plot_id AND t.jaar = pjo.jaar;
--- Import Excell sheet/6. Check in lang territoria per kavel.sql
+++ Import Excell sheet/6. Check in lang territoria per kavel.sql
@@ -2,6 +2,6 @@ SELECT
     t.plot_id, 
     p.kavel_nummer, 
     SUM(t.territoria) AS totaal_territoria
-FROM temp_waarnemingen_lang t
+FROM import_waarnemingen_lang t
 JOIN plots p ON t.plot_id = p.plot_id
 GROUP BY t.plot_id, p.kavel_nummer;
