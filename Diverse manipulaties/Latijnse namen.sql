d36f56c97bee2f2bb9fca2548b37181aad27c5c3
Ton2241 <touwen-veeboeren-5w@icloud.com>
2026-02-28 20:09:08 +0100

    update latijnse nmamemn

--- Diverse manipulaties/Latijnse namen.sql
+++ Diverse manipulaties/Latijnse namen.sql
@@ -1,6 +1,10 @@
+-- Stap 1: Selecteer euring_code, Nederlandse naam en Latijnse naam
+-- rechtstreeks uit soorten, zonder join naar een externe tabel
 SELECT 
-    s.euring_code, 
-    s.soort_naam AS soort_naam, 
-    e.latijnse_naam 
+    s.euring_code,
+    s.soort_naam AS nederlandse_naam,
+    s.latijnse_naam
 FROM soorten s
-LEFT JOIN euring e ON s.euring_code = e.euring_code;
+ORDER BY s.soort_naam ASC;
+-- Stap 2: toon alleen soorten zonder Latijnse naam
+WHERE s.latijnse_naam IS NULL OR s.latijnse_naam = ''
