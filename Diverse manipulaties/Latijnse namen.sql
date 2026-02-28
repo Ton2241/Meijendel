11a545aa6d3842490bd3dde05ac8a5c94c8203ac
Ton2241 <touwen-veeboeren-5w@icloud.com>
2026-02-28 19:11:30 +0100

    opschonen queries 1

--- Diverse manipulaties/Latijnse namen.sql
+++ Diverse manipulaties/Latijnse namen.sql
@@ -1,6 +1,6 @@
 SELECT 
     s.euring_code, 
-    s.soort_naam AS nederlandse_naam, 
+    s.soort_naam AS soort_naam, 
     e.latijnse_naam 
 FROM soorten s
 LEFT JOIN euring e ON s.euring_code = e.euring_code;
